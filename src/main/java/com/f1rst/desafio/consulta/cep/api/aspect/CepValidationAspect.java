31ccff6e918f32c0d597c43670b439bd10f6f48f
Hugo Leonardo Fernandes Noleto <hugo.noleto@totvs.com.br>
2024-12-29 20:43:01 -0300

    fix: ajusta porta Wiremock

--- src/main/java/com/f1rst/desafio/consulta/cep/api/aspect/CepValidationAspect.java
+++ src/main/java/com/f1rst/desafio/consulta/cep/api/aspect/CepValidationAspect.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 public class CepValidationAspect {
 
     @Before("execution(* com.f1rst.desafio.consulta.cep.api.service.ConsultaCepService.consultaCep(..)) && args(cep)")
-    public void validateCepFormat(String cep) {
+    public void valida(String cep) {
         if (!cep.matches("\\d{8}") && !cep.matches("\\d{5}-\\d{3}")) {
             throw new CepFormatException("CEP deve conter 8 dígitos ou 5 dígitos seguidos de hífen e mais 3 dígitos");
         }
